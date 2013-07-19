c19adf3c97fa19a0221131827658dc1c6c71bb0d
Donovan Young <dyoung522@gmail.com>
2013-07-19 17:13:43 -0400

    Add OptionsParser and split BotConfig into separate library files

--- cinchbot.rb
+++ cinchbot.rb
@@ -7,129 +7,19 @@ require 'thwait'
 require 'sequel'
 require 'cinch'
 require 'cinch/extensions/authentication'
+require_relative 'lib/optparse'
+require_relative 'lib/botconfig'
 
-# Add camelcase method to Strings
-class String
-    def camelcase
-        return self if self !~ /_/ && self =~ /[A-Z]+.*/
-        split('_').map{|e| e.capitalize}.join
-    end
-end
+$AUTHOR  = 'Donovan C. Young'
+$VERSION = 'v1.1'
 
-# Class to load and process our config file
-class BotConfig
-    Network = Struct.new( :server, :port, :ssl, :password, :nick, :nicks,
-                          :realname, :user, :messages_per_second, :server_queue_size,
-                          :strictness, :message_split_start, :message_split_end,
-                          :max_messages, :plugins, :channels, :encoding, :reconnect, :max_reconnect_delay,
-                          :local_host, :timeouts, :ping_interval, :delay_joins, :dcc, :shared, :sasl )
-
-    # Class to handle plugin files
-    class Plugin
-        def initialize( file )
-          @plugin = file
-        end
-
-        def classname
-            Object.const_get self.basename.gsub(/.rb$/, '').camelcase
-        end
-
-        def basename
-            File::basename @plugin
-        end
-    end
-
-    # Container attributes
-    attr_reader :defaults, :networks
-
-    def initialize( config_file )
-        return unless config_file
-
-        @defaults = Hash.new
-        @networks = Hash.new
-        @plugins  = {
-            core: [],
-            extra: []
-        }
-
-        begin
-            puts "Reading config from #{config_file}"
-            @config = YAML.load_file( config_file )
-
-            # Populate @defaults
-            @config['networks'].delete('defaults').each { |key, value| @defaults[key] = value }
-
-            # Load networks hash
-            @config['networks'].each { |name, details| @networks[name] = Network.new( details ) }
-
-        # Syntax Error
-        rescue Psych::SyntaxError
-            puts "Error:  There was a problem reading #{config_file}, please check it and try again"
-            return nil
-
-        # No File
-        rescue Errno::ENOENT
-            puts "Could not find #{config_file}, please create this file first."
-            return nil
-        end
-
-        # Main Bot Directory (all future paths will be relative to this, unless specified otherwise in the config)
-        dir_main = @config['dir_main'] || '.'
-
-        begin
-            Dir.chdir( File.expand_path(dir_main) )
-        rescue
-            puts "#{dir_main} does not exist.  Please check your dir_main configuration."
-            puts 'Continuing using the current directory.'
-        end
-
-        # Collect and enable plugins
-        # Load core plugins
-        plugins_core = @config['plugins']['core'] || "#{dir_main}/plugins/core"
-
-        raise "Required directory, #{plugins_core}, cannot be found.  Please check your plugins_core configuration item and try again." unless Dir.exists?(plugins_core)
-
-        puts "Loading core plugins from #{plugins_core}"
-        Dir.glob("#{plugins_core}/*.rb").each do |file| 
-            require_relative file
-            @plugins[:core] << Plugin.new(file)
-        end
-
-        begin
-            # Load optional extra plugins
-            plugins_extra = @config['plugins_extra'] || 'plugins/enabled'
-            puts "Loading extra plugins from #{plugins_extra}"
-  
-            # Find the files in the plugin_dir and load them
-            Dir.glob("#{plugins_extra}/*.rb").each do |file| 
-                require_relative file
-                @plugins[:extra] << Plugin.new(file)
-            end
-
-        rescue Errno::ENOENT
-            puts 'Whoops, looks like the plugins directory specified in the config (plugins_extra) is invalid.'
-        end
-
-        puts "#{@plugins[:extra].count > 0 ? @plugins[:extra].count : 'no'} plugins loaded."
-
-    end
-
-    ##
-    ## Methods
-    ##
-    def plugins
-        classes = Array.new
-        @plugins.keys.each { |key| @plugins[key].map { |f| classes << f.classname } }
-        return classes
-    end
-
-    def owner
-      @config['owner']
-    end
-end
+# Parse command line arguments
+$options = OptParse.parse(ARGV)
 
 # Load and process the config file
-config  = BotConfig.new( __FILE__.gsub(/.rb$/, '.yml') ) || exit
+config  = BotConfig.new( $options.conf_file ) || exit
+
+# Create a threadswait container
 threads = ThreadsWait.new
 
 # cycle through the configured networks and start our bot(s)
@@ -167,8 +57,8 @@ config.networks.each do |name, network|
             end
         end
 
-        puts "Starting connection to #{bot.config.server}"
-        bot.start
+        puts "Starting connection to #{bot.config.server}" if $options.verbose
+        bot.start unless $options.pretend
     end
 
     threads.join_nowait( thread )
@@ -176,4 +66,4 @@ end
 
 sleep 1 while threads.all_waits 
 
-puts "That's all folks."
+puts "That's all folks." if $options.verbose
---
+++ lib/optparse.rb
@@ -0,0 +1,62 @@
+require 'optparse'
+require 'ostruct'
+
+class OptParse
+  #
+  # Return a structure describing the options.
+  #
+  def self.parse(args)
+    # The options specified on the command line will be collected in *options*.
+    # We set default values here.
+    options = OpenStruct.new
+    options.conf_file = nil
+    options.verbose = false
+    options.pretend = false
+
+    opt_parser = OptionParser.new do |opts|
+      opts.banner = "Usage: #{File.basename($PROGRAM_NAME)} [options]"
+
+      opts.separator ""
+      opts.separator "Specific options:"
+
+      # Mandatory argument.
+      opts.on('-c', '--config CONFIG', 'Read our configuration items from CONFIG') do |conf|
+        options.conf_file = conf if File.exists?(conf)
+      end
+
+      # Boolean switch.
+      opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+        options.verbose = v
+      end
+
+      opts.on_tail('--pretend', "Run the program but don't actually start the bots") do
+        options.pretend = true
+      end
+
+      opts.separator ""
+      opts.separator "Common options:"
+
+      # No argument, shows at tail.  This will print an options summary.
+      # Try it and see!
+      opts.on_tail("-h", "--help", "Show this message") do
+        puts opts
+        exit
+      end
+
+      # Another typical switch to print the version.
+      opts.on_tail("--version", "Show version") do
+        puts $VERSION
+        exit
+      end
+    end
+
+    opt_parser.parse!(args)
+
+    # Set default config file if not provided above
+    options.conf_file ||= "#{File.basename($PROGRAM_NAME).gsub('.rb', '.yml')}" 
+
+    options
+  end  # parse()
+
+end  # class OptparseExample
+

fe7a2a1006a66671d02cf331d274c43bc86597c5
Donovan Young <dyoung522@gmail.com>
2013-07-30 14:14:12 -0400

    Fix superclass mismatch error in Seen plugin.

--- plugins/enabled/seen.rb
+++ plugins/enabled/seen.rb
@@ -1,15 +1,22 @@
 require 'cinch'
 
 class Seen
-  class SeenStruct < Struct.new(:who, :where, :what, :time)
+  include Cinch::Plugin
+
+  # Create the new Struct class, then monkey-patch it.  This is necessary so we don't end up
+  # with a superclass mismatch if it's reloaded.
+  SeenStruct = Struct.new(:who, :where, :what, :time)
+  class SeenStruct
     def to_s
       "[#{time.asctime}] #{who} was seen in #{where} saying #{what}"
     end
   end
 
-  include Cinch::Plugin
+  set :plugin_name, 'seen'
+  set :help, "Usage: !#{self.plugin_name} <nick> : Displays last seen information for <nick>"
+
   listen_to :channel
-  match /seen (.+)/
+  match /seen (\S+)/
 
   def initialize(*args)
     super
@@ -23,12 +30,16 @@ class Seen
   def execute(m, nick)
     if nick.downcase == @bot.nick.downcase
       m.reply "That's me!"
+
     elsif nick.downcase == m.user.nick.downcase
       m.reply "That's you!"
+
     elsif @users.key?(nick.downcase)
       m.reply @users[nick.downcase].to_s
+
     else
       m.reply "I haven't seen #{nick}"
+
     end
   end
 end
